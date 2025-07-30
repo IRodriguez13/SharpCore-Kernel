1c090d0330e3e42a10b4b447dc53de30942cc0f4
Iván Rodriguez <ivanrwcm25@gmail.com>
2025-07-30 00:18:04 -0300

    [TRANSLATE] -Translating source code files
    Signed-off-by: Iván Rodriguez <ivanrwcm25@gmail.com>

--- ShpCore.Staging/StagingManager.cs
+++ ShpCore.Staging/StagingManager.cs
@@ -24,12 +24,12 @@ public class StagingManager<T>
         try
         {
             _applyCallback(_current); // Aplicamos el cambio actual
-            KernelLog.Debug("[Staging] Instruccion aplicada");
+            KernelLog.Debug("[Staging] Instruction added succesfully");
         }
         catch (Exception)
         {
             MSrevert(); // Automático por si la aplicación falla
-            KernelLog.Panic("[Staging] Error al aplicar la instrucción. Se ha revertido el cambio.");
+            KernelLog.Panic("[Staging StagingManager line:32] Err with instruction commit, Rollbacking...");
 
             _current = default;
             _history.Clear();
@@ -41,18 +41,18 @@ public class StagingManager<T>
     {
         if (_history.Count == 0)
         {
-            KernelLog.Panic("[Staging] No hay versiones previas para hacer rollback.");
+            KernelLog.Panic("[Staging Staging	Manager line:44 ] No previous verions to rollback.");
             return;
         }
 
         // Rollbackear sería limpiar la pila y volver al último estado válido
-        KernelLog.Debug("[Staging] Revirtiendo al último estado válido");
+        KernelLog.Debug("[Staging] Reverting to last valid status");
 
         _current = _history.Pop();
         _rollbackCallback(_current);
 
-        KernelLog.Debug("[Staging] Rollback exitoso");
-        KernelLog.Info($"[Staging] Estado actual: {_current}");
+        KernelLog.Debug("[Staging] Rollback OK");
+        KernelLog.Info($"[Staging] Current Status: {_current}");
     }
 
     // Confirmamos estado actual como final. Aca el commit es simplemente dejar current como está y limpiar la pila.
